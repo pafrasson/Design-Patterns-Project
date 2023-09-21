1dfb440844d42417509419736325c43fddf91422
pafrasson <pafrasson@gmail.com>
2023-09-21 08:48:02 -0300

    first commit

---
+++ src/dominio/Pessoa.java
@@ -0,0 +1,13 @@
+package dominio;
+
+public class Pessoa {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Pessoa(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
---
+++ src/test/BuilderPatternTest01.java
@@ -0,0 +1,9 @@
+package test;
+
+import dominio.Pessoa;
+
+public class BuilderPatternTest01 {
+    public static void main(String[] args) {
+        new Pessoa("Pedro","Frasson","");
+    }
+}
